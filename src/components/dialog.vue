a0b2cf4205834828f5fb8946a66f7d045c92a788
Filish .K <filishope@163.com>
2021-02-20 16:13:31 +0800

    init

---
+++ src/components/dialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+interface Person {
+  name: string;
+  class: string;
+  age: number;
+}
+
+export default defineComponent({
+  props: {
+    success: { type: String },
+    callback: {
+      type: Function as PropType<() => void>
+    },
+    student: {
+      type: Object as PropType<Student>,
+      required: true
+    }
+  },
+  setup() {},
+  data() {
+    return {
+      message: "Vue3 code style"
+    };
+  },
+  computed: {
+    reversedMessage(): string {
+      return this.message
+        .split(" ")
+        .reverse()
+        .join("");
+    }
+  }
+});
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Dialog msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Dialog from "@/components/dialog.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld
+    Dialog
   }
 })
-export default class Home extends Vue {}
 </script>
